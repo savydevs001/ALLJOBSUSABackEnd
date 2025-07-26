ec25a63b3d89a575d82f676274da237d4229d97f
Muhammad Ahmad Shahid <mahmads0323@gmail.com>
2025-07-26 10:44:15 +0500

    upload

--- src/middlewares/uploadFile.middleware.js
+++ src/middlewares/uploadFile.middleware.js
@@ -2,6 +2,12 @@ import multer from "multer";
 import path from "path";
 
 const folder = "src/public/uploads/";
+
+const uploadDir = path.join(__dirname, folder);
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, folder);
