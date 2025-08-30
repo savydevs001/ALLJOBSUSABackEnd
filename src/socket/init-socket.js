4f3481da04c18a005e3a0fb35222fb7de418a6cb
Muhammad Ahmad Shahid <mahmads0323@gmail.com>
2025-08-31 00:25:18 +0500

    b:ok

--- src/socket/init-socket.js
+++ src/socket/init-socket.js
@@ -11,10 +11,7 @@ import {
 } from "./onlineUsers.js";
 import mongoose from "mongoose";
 import SupportMessage from "../database/models/support-message.model.js";
-import {
-  getSupportAdminId,
-  getSupportIds,
-} from "../controllers/support.controller.js";
+import { getSupportIds } from "../controllers/support.controller.js";
 
 dotenv.config();
 
@@ -45,155 +42,215 @@ const initSocket = (httpServer) => {
   });
 
   io.on("connection", (socket) => {
-    const userId = socket.user?._id;
-    const role =
-      socket.user?.role == "employer"
-        ? "employer"
-        : socket.user?.role == "freelancer"
-        ? "freelancer"
-        : socket.user?.role == "job-seeker"
-        ? "job-seeker"
-        : socket.user?.role == "admin"
-        ? "admin"
-        : socket?.user.role == "manager"
-        ? "manager"
-        : "";
+    try {
+      const userId = socket.user?._id;
+      const role =
+        socket.user?.role == "employer"
+          ? "employer"
+          : socket.user?.role == "freelancer"
+          ? "freelancer"
+          : socket.user?.role == "job-seeker"
+          ? "job-seeker"
+          : socket.user?.role == "admin"
+          ? "admin"
+          : socket?.user.role == "manager"
+          ? "manager"
+          : "";
 
-    if (
-      !role ||
-      !userId ||
-      !mongoose.Types.ObjectId.isValid(userId) ||
-      role == ""
-    ) {
-      return;
-    }
-
-    refreshOnlineUser(userId, socket.id); // First registration
-    console.log(`✅ ${userId} connected`);
-
-    socket.broadcast.emit("user-online", { userId });
-
-    socket.on("message", async ({ to, content, fileName, fileUrl }) => {
-      await handleMessage({ to, content, fileName, fileUrl, userId: userId });
-    });
-
-    socket.on("message-seen", async ({ messageId }) => {
-      try {
-        console.log("messageId: ", messageId);
-        await Message.updateOne({ _id: messageId }, { seen: true });
-      } catch (err) {
-        console.log("Error updating message to seen");
+      if (
+        !role ||
+        !userId ||
+        !mongoose.Types.ObjectId.isValid(userId) ||
+        role == ""
+      ) {
+        return;
       }
-    });
 
-    socket.on(
-      "support-message",
-      async ({
-        ticketId,
-        message,
-        fileName,
-        fileUrl,
-        receiverId,
-        mode = "from_user_to_support",
-      }) => {
-        /***
-         * 2 Modes
-         *  mode = "from_user_to_support"
-         * mode = "from_support_to_user"
-         */
+      refreshOnlineUser(userId, socket.id); // First registration
+      console.log(`✅ ${userId} connected`);
+
+      socket.broadcast.emit("user-online", { userId });
+
+      socket.on("message", async ({ to, content, fileName, fileUrl }) => {
         try {
-          if (mode === "from_user_to_support") {
-            if (!userId || !mongoose.Types.ObjectId.isValid(userId)) {
-              console.log("Invalid userId");
-              return;
-            }
-          }
+          await handleMessage({
+            to,
+            content,
+            fileName,
+            fileUrl,
+            userId: userId,
+          });
+        } catch (err) {
+          console.log("Error on message " + err);
+          socket.emit("error", {
+            message: "Error sending message",
+            err: err,
+          });
+        }
+      });
 
-          if (!ticketId) {
-            console.log("Invalid Ticket Id");
-            return;
-          }
+      socket.on("message-seen", async ({ messageId }) => {
+        try {
+          console.log("messageId: ", messageId);
+          await Message.updateOne({ _id: messageId }, { seen: true });
+        } catch (err) {
+          console.log("Error updating message to seen");
+          socket.emit("error", {
+            message: "Error updaing  message to seen",
+            err: err,
+          });
+        }
+      });
 
-          const details = {
-            ticketId: ticketId,
-            message: message,
-            seen: mode == false,
-          };
-          if (fileName && fileUrl) {
-            details.attachments = {
-              fileName,
-              fileUrl,
-            };
-          }
-
-          if (mode === "from_user_to_support") {
-            details.senderId = userId;
-          } else if (mode === "from_support_to_user") {
-            details.receiverId = receiverId;
-          } else {
-            console.log("❌ Invlaid mode for support chat");
-            return;
-          }
-
-          const supportMessage = new SupportMessage(details);
-          await supportMessage.save();
-
-          const sender = getOnlineUser(userId);
-          if (sender && sender.socketId) {
-            io.to(sender.socketId).emit("support-message", supportMessage);
-          }
-
-          let tempReceiverIds;
-          if (mode === "from_user_to_support") {
-            tempReceiverIds = await getSupportIds();
-          } else {
-            tempReceiverIds = [receiverId];
-          }
-
-          const receivers = tempReceiverIds.map((e) => getOnlineUser(e));
-          if (receivers.length > 0) {
-            for (const rec of receivers) {
-              if (rec && rec.socketId) {
-                io.to(rec.socketId).emit("support-message", supportMessage);
+      socket.on(
+        "support-message",
+        async ({
+          ticketId,
+          message,
+          fileName,
+          fileUrl,
+          receiverId,
+          mode = "from_user_to_support",
+        }) => {
+          /***
+           * 2 Modes
+           *  mode = "from_user_to_support"
+           * mode = "from_support_to_user"
+           */
+          try {
+            if (mode === "from_user_to_support") {
+              if (!userId || !mongoose.Types.ObjectId.isValid(userId)) {
+                console.log("Invalid userId");
+                return;
               }
             }
+
+            if (!ticketId) {
+              console.log("Invalid Ticket Id");
+              return;
+            }
+
+            const details = {
+              ticketId: ticketId,
+              message: message,
+              seen: mode == false,
+            };
+            if (fileName && fileUrl) {
+              details.attachments = {
+                fileName,
+                fileUrl,
+              };
+            }
+
+            if (mode === "from_user_to_support") {
+              details.senderId = userId;
+            } else if (mode === "from_support_to_user") {
+              details.receiverId = receiverId;
+            } else {
+              console.log("❌ Invlaid mode for support chat");
+              return;
+            }
+
+            const supportMessage = new SupportMessage(details);
+            await supportMessage.save();
+
+            const sender = getOnlineUser(userId);
+            if (sender && sender.socketId) {
+              io.to(sender.socketId).emit("support-message", supportMessage);
+            }
+
+            let tempReceiverIds;
+            if (mode === "from_user_to_support") {
+              tempReceiverIds = await getSupportIds();
+            } else {
+              tempReceiverIds = [receiverId];
+            }
+
+            const receivers = tempReceiverIds.map((e) => getOnlineUser(e));
+            if (receivers.length > 0) {
+              for (const rec of receivers) {
+                if (rec && rec.socketId) {
+                  io.to(rec.socketId).emit("support-message", supportMessage);
+                }
+              }
+            }
+          } catch (err) {
+            console.log("Error Sending support message: ", err);
+            socket.emit("error", {
+              message: "Error sending support message",
+              err: err,
+            });
+          }
+        }
+      );
+
+      socket.on("support-message-seen", async ({ messageId }) => {
+        try {
+          await SupportMessage.updateOne({ _id: messageId }, { seen: true });
+        } catch (err) {
+          console.log("Error updating support message to seen");
+          socket.emit("error", {
+            message: "Error updaing support message to seen",
+            err: err,
+          });
+        }
+      });
+
+      socket.on("online", (userIds) => {
+        try {
+          if (Array.isArray(userIds)) {
+            let onlineUsers = [];
+            if (userIds && userIds.length > 0) {
+              onlineUsers = userIds.filter((id) => checkOnlineUser(id));
+            }
+            socket.emit("online", onlineUsers);
+          } else {
+            console.log("User Ids should be array");
           }
         } catch (err) {
-          console.log("Error Sending support message: ", err);
-          return;
+          console.log("Error on online users " + err);
+          socket.emit("error", {
+            message: "UserIds must be a string array",
+            err: err,
+          });
         }
+      });
+
+      socket.on("single-online", ({ userId }) => {
+        try {
+          const online = checkOnlineUser(userId);
+          socket.emit("single-online", { online });
+        } catch (err) {
+          console.log("Error on single online user " + err);
+          socket.emit("error", {
+            message: "Error adding a user to online list",
+            err: err,
+          });
+        }
+      });
+
+      socket.on("disconnect", () => {
+        try {
+          console.log(`❌ ${userId} dis-connected`);
+          deleteOnlineUser(userId);
+          socket.broadcast.emit("user-offline", { userId });
+        } catch (err) {
+          console.log("Error on disconect user " + err);
+          socket.emit("error", {
+            message: "Error disconnecting user",
+            err: err,
+          });
+        }
+      });
+    } catch (err) {
+      if (socket?.connected) {
+        socket.emit("error", {
+          message: "Unknown error",
+          err: err,
+        });
       }
-    );
-
-    socket.on("support-message-seen", async ({ messageId }) => {
-      try {
-        console.log("ok: ", messageId);
-        await SupportMessage.updateOne({ _id: messageId }, { seen: true });
-      } catch (err) {
-        console.log("Error updating support message to seen");
-      }
-    });
-
-    socket.on("online", (userIds) => {
-      let onlineUsers = [];
-      if (userIds && userIds.length > 0) {
-        onlineUsers = userIds.filter((id) => checkOnlineUser(id));
-      }
-      // console.log("online: ", onlineUsers)
-      socket.emit("online", onlineUsers);
-    });
-
-    socket.on("single-online", ({ userId }) => {
-      const online = checkOnlineUser(userId);
-      console.log("single-online: " + userId + " " + online);
-      socket.emit("single-online", { online });
-    });
-
-    socket.on("disconnect", () => {
-      console.log(`❌ ${userId} dis-connected`);
-      deleteOnlineUser(userId);
-      socket.broadcast.emit("user-offline", { userId });
-    });
+      console.log("Unkknow socket error: ", err);
+    }
   });
 };
 
@@ -255,6 +312,10 @@ const handleMessage = async ({
     }
   } catch (err) {
     console.log("Error Sending Message: ", err);
+    socket.emit("error", {
+      message: "Error sending message",
+      err: err,
+    });
   }
 };
 
