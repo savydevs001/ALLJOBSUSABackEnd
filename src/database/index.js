e5613df3c1035e6a72f20844d6803a482ee467a9
Muhammad Ahmad Shahid <mahmads0323@gmail.com>
2025-10-18 13:08:49 +0500

    database update

--- src/database/index.js
+++ src/database/index.js
@@ -4,15 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017";
-const dbName = "ALLJOBSUSA_DB";
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(MONGODB_URI, {
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true,
-      dbName: dbName,
-    });
+    await mongoose.connect(MONGODB_URI);
     console.log("✅ Connected to MongoDB via Mongoose");
   } catch (err) {
     console.error("❌ Failed to connect to MongoDB:", err.message);
